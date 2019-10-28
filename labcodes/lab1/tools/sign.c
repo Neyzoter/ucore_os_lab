ddc19dc902b31c929751485449f016e344f900d5
Neyzoter <sonechaochao@gmail.com>
2019-10-28 17:15:01 +0800

    update

--- labcodes/lab1/tools/sign.c
+++ labcodes/lab1/tools/sign.c
@@ -6,10 +6,13 @@
 int
 main(int argc, char *argv[]) {
     struct stat st;
+    // [scc] 判断参数个数
     if (argc != 3) {
         fprintf(stderr, "Usage: <input filename> <output filename>\n");
         return -1;
     }
+    // [scc] arg[0]：程序路径运行的全路径名
+    //       arg[1]：第一个参数，打开文件错误
     if (stat(argv[1], &st) != 0) {
         fprintf(stderr, "Error opening file '%s': %s\n", argv[1], strerror(errno));
         return -1;
@@ -22,16 +25,21 @@ main(int argc, char *argv[]) {
     char buf[512];
     memset(buf, 0, sizeof(buf));
     FILE *ifp = fopen(argv[1], "rb");
+    // [scc] 目标buf；每个对象的大小（字节为单位）；要读取的对象的个数；输入流
     int size = fread(buf, 1, st.st_size, ifp);
+    // [scc] 验证实际读取读取的个数和st.st_size是否一样
     if (size != st.st_size) {
         fprintf(stderr, "read '%s' error, size is %d.\n", argv[1], size);
         return -1;
     }
     fclose(ifp);
+    // buf最后两个字节必须是0x55和0xAA
     buf[510] = 0x55;
     buf[511] = 0xAA;
+    // [scc] 写入到argv[2]
     FILE *ofp = fopen(argv[2], "wb+");
     size = fwrite(buf, 1, 512, ofp);
+    // [scc] 写入文件的大小必须是512字节
     if (size != 512) {
         fprintf(stderr, "write '%s' error, size is %d.\n", argv[2], size);
         return -1;
