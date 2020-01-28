c4cd8c8a9a3279ba8543f31d3f74c66d62b14029
Neyzoter <sonechaochao@gmail.com>
2020-01-28 14:30:07 +0800

    add test

--- labcodes_answer/lab5_result/user/hello.c
+++ labcodes_answer/lab5_result/user/hello.c
@@ -6,6 +6,15 @@ main(void) {
     cprintf("Hello world!!.\n");
     cprintf("I am process %d.\n", getpid());
     cprintf("hello pass.\n");
+    test(1,2,3,4);
     return 0;
+}
+
+// [LAB5 SCC] test stack
+int test(val1,val2,val3,val4) {
+    cprintf("val1 ptr : %d\n", &val1) ;
+    cprintf("val2 ptr : %d\n", &val2) ;
+    cprintf("val3 ptr : %d\n", &val3) ;
+    cprintf("val4 ptr : %d\n", &val4) ;
 }
 
